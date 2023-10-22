6b41865d33141598bc4f537e677b077393d0e488
isurusachinda85 <isurusachintha8@gmail.com>
2023-10-23 05:05:37 +0530

    update

--- 02_Back_End/src/main/java/lk/ijse/carRentSystem/controllers/CustomerControllers.java
+++ 02_Back_End/src/main/java/lk/ijse/carRentSystem/controllers/CustomerControllers.java
@@ -1,10 +1,18 @@
 package lk.ijse.carRentSystem.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/customer")
 public class CustomerControllers {
     public CustomerControllers() {
         System.out.println("Customer");
+    }
+
+    @GetMapping
+    public String saveCustomer(){
+        return "save";
     }
 }
