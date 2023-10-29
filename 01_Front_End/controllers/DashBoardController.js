6f9c68d26262a54a04e59eda469a2e5200be1b1b
isurusachinda85 <isurusachintha8@gmail.com>
2023-10-29 15:32:28 +0530

    update front end

---
+++ 01_Front_End/controllers/DashBoardController.js
@@ -0,0 +1,83 @@
+initiateUI();
+
+function initiateUI() {
+    clearAll();
+    $("#homeForm").css("display", "block");
+    setTheLastView();
+}
+
+function saveLastView(clickedID) {
+    switch (clickedID) {
+        case "homeForm":
+            localStorage.setItem("view", "HOME");
+            break;
+        case "customerForm":
+            localStorage.setItem("view", "CUSTOMER");
+            break;
+        case "itemForm":
+            localStorage.setItem("view", "ITEM");
+            break;
+        case "placeOrderForm":
+            localStorage.setItem("view", "ORDER");
+            break;
+
+        case "orderDetailForm":
+            localStorage.setItem("view", "ORDERDETAIL");
+            break;
+    }
+}
+
+function setTheLastView() {
+    let view = localStorage.getItem("view");
+    switch (view) {
+        case "HOME":
+            setView($("#homeForm"));
+            break;
+        case "ITEM":
+            setView($("#itemForm"));
+            break;
+        case "CUSTOMER":
+            setView($("#customerForm"));
+            break;
+        case "ORDER":
+            setView($("#placeOrderForm"));
+            break;
+        case "ORDERDETAIL":
+            setView($("#orderDetailForm"));
+            break;
+        default:
+            setView($("#homeForm"));
+    }
+}
+
+function clearAll() {
+    $("#homeForm,#customerForm,#itemForm,#placeOrderForm,#orderDetailForm").css('display', 'none');
+}
+
+function setView(viewOb) {
+    clearAll();
+    viewOb.css("display", "block");
+    saveLastView(viewOb.get(0).id);
+    // console.log(viewOb.get(0).id);
+}
+
+//bind events
+$("#Home").click(function () {
+    setView($("#homeForm"));
+});
+
+$("#Customer").click(function () {
+    setView($("#customerForm"));
+});
+
+$("#Item").click(function () {
+    setView($("#itemForm"));
+});
+
+$("#PlaceOrder").click(function () {
+    setView($("#placeOrderForm"));
+});
+
+$("#OrderDetail").click(function () {
+    setView($("#orderDetailForm"));
+});
